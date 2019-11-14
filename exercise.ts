ff1b89e1607217610e690693518c4f12c2cabf79
Beau Witter <bwitter@risklens.com>
2019-11-14 14:38:50 -0800

    WorkItem-27: Types the exercise as explicitly as possible

--- exercise.ts
+++ exercise.ts
@@ -1,11 +1,15 @@
-let bankAccount = {
+type BankAccount = { money: number; deposit: (val: number) => void };
+
+let bankAccount: BankAccount = {
     money: 2000,
     deposit(value) {
         this.money += value;
     }
 };
 
-let myself = {
+type Person = { name: string; bankAccount: BankAccount; hobbies: string[] };
+
+let myself: Person = {
     name: "Beau",
     bankAccount: bankAccount,
     hobbies: ["Cooking", "Video Games"]
