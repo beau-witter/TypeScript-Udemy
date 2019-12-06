4d5cc742aeb0b8bfa6655fc1aaaa1a24bbeccc5f
Beau Witter <bwitter@risklens.com>
2019-12-06 14:03:48 -0800

    WorkItem-14: type inference

--- Session2/app.ts
+++ Session2/app.ts
@@ -12,10 +12,11 @@
         }
     }
 
-    const number1 = 5;
+    let number1: number;
+    number1 = 5;
     const number2 = 2.8;
     const printResult = true;
-    const resultPhrase = "Result is: ";
+    let resultPhrase = "Result is: ";
 
     add(number1, number2, printResult, resultPhrase);
 }
