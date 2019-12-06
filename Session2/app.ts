c2587e1893772b8253a3e9fd8d85fd98feb474c1
Beau Witter <bwitter@risklens.com>
2019-12-06 14:32:17 -0800

    WorkItem-18: tuples

--- Session2/app.ts
+++ Session2/app.ts
@@ -3,11 +3,22 @@
     //     name: string;
     //     age: number;
     // } = {
-    const person = {
+    const person: {
+        name: string;
+        age: number;
+        hobbies: string[];
+        role: [number, string];
+    } = {
         name: "Beau",
         age: 27,
-        hobbies: ["Games", "Cooking"]
+        hobbies: ["Games", "Cooking"],
+        role: [2, "author"]
     };
+
+    //person.role.push("admin"); // Exception to the rule
+    //person.role[1] = 10;
+
+    //person.role = [0, "admin", "user"];
 
     let favoriteActivites: string[];
     favoriteActivites = ["Sports"];
