7db1acf7ce16edd41c3148fd18650babe9a6b08d
Beau Witter <bwitter@risklens.com>
2019-12-06 14:26:51 -0800

    WorkItem-17: arrays

--- Session2/app.ts
+++ Session2/app.ts
@@ -1,12 +1,21 @@
 {
-    const person: {
-        name: string;
-        age: number;
-    } = {
-        //const person = {
+    // const person: {
+    //     name: string;
+    //     age: number;
+    // } = {
+    const person = {
         name: "Beau",
-        age: 27
+        age: 27,
+        hobbies: ["Games", "Cooking"]
     };
 
+    let favoriteActivites: string[];
+    favoriteActivites = ["Sports"];
+
     console.log(person.name);
+
+    for (const hobby of person.hobbies) {
+        console.log(hobby.toUpperCase());
+        //console.log(hobby.map()); // !!! ERROR !!!
+    }
 }
